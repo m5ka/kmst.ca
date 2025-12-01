ddf3467bfebb9ffc6087107d0812423b197a6e40
m5ka <m5ka+github@posteo.de>
2025-12-01 02:22:21 +0200

    chore: 📜 Update production logging settings

--- kmstca/settings/production.py
+++ kmstca/settings/production.py
@@ -6,16 +6,26 @@ DEBUG = False
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "bucket_name": os.environ["KMSTCA_STORAGE_BUCKET_USER_CONTENT"],
-        },
+        "OPTIONS": {"bucket_name": os.environ["KMSTCA_STORAGE_BUCKET_USER_CONTENT"]},
     },
     "staticfiles": {
         "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "bucket_name": os.environ["KMSTCA_STORAGE_BUCKET_STATIC"],
-        },
+        "OPTIONS": {"bucket_name": os.environ["KMSTCA_STORAGE_BUCKET_STATIC"]},
     },
 }
 AWS_S3_ACCESS_KEY_ID = os.environ["KMSTCA_S3_ACCESS_KEY"]
 AWS_S3_SECRET_ACCESS_KEY = os.environ["KMSTCA_S3_SECRET_KEY"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "{levelname} {asctime} {module} {message}", "style": "{"},
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "verbose"},
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "DEBUG", "propagate": True}
+    },
+}
